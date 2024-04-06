89ac7c2e55c0ea7bd3b9be817007ab3ff53c5821
Sang1209 <trinhhoangphuocsang250503@gmail.com>
2024-04-06 15:46:14 +0700

    abc

--- src/main/java/com/aptech/ticketshow/services/EventService.java
+++ src/main/java/com/aptech/ticketshow/services/EventService.java
@@ -1,9 +1,19 @@
 package com.aptech.ticketshow.services;
 
 import com.aptech.ticketshow.data.dtos.EventDTO;
+import com.aptech.ticketshow.data.dtos.TicketDTO;
+
 import java.util.List;
 
 public interface EventService {
 
     List<EventDTO> findAll();
+
+    EventDTO findByID(Long id);
+
+    EventDTO addEvent(EventDTO eventDTO);
+
+    void deleteEvent(Long id);
+
+    EventDTO updateEvent(EventDTO eventDTO);
 }
