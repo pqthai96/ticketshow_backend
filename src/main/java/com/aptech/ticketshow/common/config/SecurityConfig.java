4801ef713c217180787b85753cf7eff81056e88a
Quoc Thai <pqthai96@gmail.com>
2025-03-07 00:29:14 +0700

    new change 12

--- src/main/java/com/aptech/ticketshow/common/config/SecurityConfig.java
+++ src/main/java/com/aptech/ticketshow/common/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig implements WebMvcConfigurer {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("http://localhost:3001");
-        config.addAllowedOrigin("https://ticketshow-user-1hyjtrmsr-phan-quoc-thais-projects.vercel.app");
+        config.addAllowedOrigin("https://ticketshow-user-app.vercel.app/");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
